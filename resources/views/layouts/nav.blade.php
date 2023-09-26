b2c2bef0b6017fb4589851d8e521fb157c7696bf
aditbest5 <44321384+aditbest5@users.noreply.github.com>
2023-09-26 17:00:43 +0700

    perubahan navbar

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,69 +1,126 @@
 <div id="layoutSidenav_nav">
-                <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
-                    <div class="sb-sidenav-menu">
-                        <div class="nav">
-                            <div class="sb-sidenav-menu-heading">Core</div>
-                            <a class="nav-link" href="index.html">
-                                <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
-                                Dashboard
-                            </a>
-                            <div class="sb-sidenav-menu-heading">Interface</div>
-                            <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
-                                <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
-                                Layouts
-                                <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-                            </a>
-                            <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
-                                <nav class="sb-sidenav-menu-nested nav">
-                                    <a class="nav-link" href="layout-static.html">Static Navigation</a>
-                                    <a class="nav-link" href="layout-sidenav-light.html">Light Sidenav</a>
-                                </nav>
-                            </div>
-                            <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapsePages" aria-expanded="false" aria-controls="collapsePages">
-                                <div class="sb-nav-link-icon"><i class="fas fa-book-open"></i></div>
-                                Pages
-                                <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-                            </a>
-                            <div class="collapse" id="collapsePages" aria-labelledby="headingTwo" data-bs-parent="#sidenavAccordion">
-                                <nav class="sb-sidenav-menu-nested nav accordion" id="sidenavAccordionPages">
-                                    <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#pagesCollapseAuth" aria-expanded="false" aria-controls="pagesCollapseAuth">
-                                        Authentication
-                                        <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-                                    </a>
-                                    <div class="collapse" id="pagesCollapseAuth" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordionPages">
-                                        <nav class="sb-sidenav-menu-nested nav">
-                                            <a class="nav-link" href="login.html">Login</a>
-                                            <a class="nav-link" href="register.html">Register</a>
-                                            <a class="nav-link" href="password.html">Forgot Password</a>
-                                        </nav>
-                                    </div>
-                                    <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#pagesCollapseError" aria-expanded="false" aria-controls="pagesCollapseError">
-                                        Error
-                                        <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-                                    </a>
-                                    <div class="collapse" id="pagesCollapseError" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordionPages">
-                                        <nav class="sb-sidenav-menu-nested nav">
-                                            <a class="nav-link" href="401.html">401 Page</a>
-                                            <a class="nav-link" href="404.html">404 Page</a>
-                                            <a class="nav-link" href="500.html">500 Page</a>
-                                        </nav>
-                                    </div>
-                                </nav>
-                            </div>
-                            <div class="sb-sidenav-menu-heading">Addons</div>
-                            <a class="nav-link" href="charts.html">
-                                <div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
-                                Charts
-                            </a>
-                            <a class="nav-link" href="tables.html">
-                                <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
-                                Tables
-                            </a>
+    <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+        <div class="sb-sidenav-menu">
+            <div class="nav">
+                <div class="sb-sidenav-menu-heading">MAIN MENU</div>
+                <a class="nav-link" href="index.html">
+                    <div class="sb-nav-link-icon"><svg width="20" height="20" viewBox="0 0 20 20" fill="none"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M15.5585 1.66666H13.9752C12.1585 1.66666 11.2002 2.62499 11.2002 4.44166V6.02499C11.2002 7.84166 12.1585 8.79999 13.9752 8.79999H15.5585C17.3752 8.79999 18.3335 7.84166 18.3335 6.02499V4.44166C18.3335 2.62499 17.3752 1.66666 15.5585 1.66666Z"
+                                fill="#7D7D7D" />
+                            <path
+                                d="M6.03366 11.1917H4.45033C2.62533 11.1917 1.66699 12.15 1.66699 13.9667V15.55C1.66699 17.375 2.62533 18.3333 4.44199 18.3333H6.02533C7.84199 18.3333 8.80033 17.375 8.80033 15.5583V13.975C8.80866 12.15 7.85033 11.1917 6.03366 11.1917Z"
+                                fill="#7D7D7D" />
+                            <path
+                                d="M5.24199 8.81666C7.21641 8.81666 8.81699 7.21607 8.81699 5.24166C8.81699 3.26724 7.21641 1.66666 5.24199 1.66666C3.26757 1.66666 1.66699 3.26724 1.66699 5.24166C1.66699 7.21607 3.26757 8.81666 5.24199 8.81666Z"
+                                fill="#7D7D7D" />
+                            <path
+                                d="M14.7586 18.3333C16.733 18.3333 18.3336 16.7327 18.3336 14.7583C18.3336 12.7839 16.733 11.1833 14.7586 11.1833C12.7842 11.1833 11.1836 12.7839 11.1836 14.7583C11.1836 16.7327 12.7842 18.3333 14.7586 18.3333Z"
+                                fill="#7D7D7D" />
+                        </svg>
+                    </div>
+                    Overview
+                </a>
+                <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseData"
+                    aria-expanded="false" aria-controls="collapseData">
+                    <div class="sb-nav-link-icon"><svg width="20" height="20" viewBox="0 0 20 20" fill="none"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M13.3333 18.9583H6.66665C2.81665 18.9583 2.09999 17.1667 1.91665 15.425L1.29165 8.75C1.19999 7.875 1.17499 6.58333 2.04165 5.61666C2.79165 4.78333 4.03332 4.38333 5.83332 4.38333H14.1667C15.975 4.38333 17.2167 4.79166 17.9583 5.61666C18.825 6.58333 18.8 7.875 18.7083 8.75833L18.0833 15.4167C17.9 17.1667 17.1833 18.9583 13.3333 18.9583ZM5.83332 5.625C4.42499 5.625 3.45832 5.9 2.96665 6.45C2.55832 6.9 2.42499 7.59166 2.53332 8.625L3.15832 15.3C3.29999 16.6167 3.65832 17.7083 6.66665 17.7083H13.3333C16.3333 17.7083 16.7 16.6167 16.8417 15.2917L17.4667 8.63333C17.575 7.59166 17.4417 6.9 17.0333 6.45C16.5417 5.9 15.575 5.625 14.1667 5.625H5.83332Z"
+                                fill="white" />
+                            <path
+                                d="M13.3334 5.62499C12.9917 5.62499 12.7084 5.34166 12.7084 4.99999V4.33332C12.7084 2.84999 12.7084 2.29166 10.6667 2.29166H9.33341C7.29175 2.29166 7.29175 2.84999 7.29175 4.33332V4.99999C7.29175 5.34166 7.00841 5.62499 6.66675 5.62499C6.32508 5.62499 6.04175 5.34166 6.04175 4.99999V4.33332C6.04175 2.86666 6.04175 1.04166 9.33341 1.04166H10.6667C13.9584 1.04166 13.9584 2.86666 13.9584 4.33332V4.99999C13.9584 5.34166 13.6751 5.62499 13.3334 5.62499Z"
+                                fill="white" />
+                            <path
+                                d="M9.99992 13.9583C7.70825 13.9583 7.70825 12.5417 7.70825 11.6917V10.8333C7.70825 9.65833 7.99159 9.375 9.16659 9.375H10.8333C12.0083 9.375 12.2916 9.65833 12.2916 10.8333V11.6667C12.2916 12.5333 12.2916 13.9583 9.99992 13.9583ZM8.95825 10.625C8.95825 10.6917 8.95825 10.7667 8.95825 10.8333V11.6917C8.95825 12.55 8.95825 12.7083 9.99992 12.7083C11.0416 12.7083 11.0416 12.575 11.0416 11.6833V10.8333C11.0416 10.7667 11.0416 10.6917 11.0416 10.625C10.9749 10.625 10.8999 10.625 10.8333 10.625H9.16659C9.09992 10.625 9.02492 10.625 8.95825 10.625Z"
+                                fill="white" />
+                            <path
+                                d="M11.6667 12.3083C11.3584 12.3083 11.0834 12.075 11.0501 11.7583C11.0084 11.4167 11.2501 11.1 11.5917 11.0583C13.7917 10.7833 15.9001 9.95001 17.6751 8.65834C17.9501 8.45001 18.3417 8.51667 18.5501 8.80001C18.7501 9.07501 18.6917 9.46667 18.4084 9.67501C16.4584 11.0917 14.1584 12 11.7417 12.3083C11.7167 12.3083 11.6917 12.3083 11.6667 12.3083Z"
+                                fill="white" />
+                            <path
+                                d="M8.33333 12.3167C8.30833 12.3167 8.28333 12.3167 8.25833 12.3167C5.97499 12.0583 3.74999 11.225 1.82499 9.90833C1.54166 9.71667 1.46666 9.325 1.65833 9.04167C1.84999 8.75833 2.24166 8.68333 2.52499 8.875C4.28333 10.075 6.30833 10.8333 8.39166 11.075C8.73333 11.1167 8.98333 11.425 8.94166 11.7667C8.91666 12.0833 8.64999 12.3167 8.33333 12.3167Z"
+                                fill="white" />
+                        </svg>
+                    </div>
+                    Data
+                    <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                </a>
+                <div class="collapse" id="collapseData" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+                    <nav class="sb-sidenav-menu-nested nav">
+                        <a class="nav-link" href="layout-static.html">Department</a>
+                        <a class="nav-link" href="layout-sidenav-light.html">Division</a>
+                        <a class="nav-link" href="layout-sidenav-light.html">Job Title</a>
+                        <a class="nav-link" href="layout-sidenav-light.html">Status</a>
+                    </nav>
+                </div>
+                <div class="sb-sidenav-menu-heading">Interface</div>
+                <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts"
+                    aria-expanded="false" aria-controls="collapseLayouts">
+                    <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
+                    Layouts
+                    <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                </a>
+                <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne"
+                    data-bs-parent="#sidenavAccordion">
+                    <nav class="sb-sidenav-menu-nested nav">
+                        <a class="nav-link" href="layout-static.html">Static Navigation</a>
+                        <a class="nav-link" href="layout-sidenav-light.html">Light Sidenav</a>
+                    </nav>
+                </div>
+                <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapsePages"
+                    aria-expanded="false" aria-controls="collapsePages">
+                    <div class="sb-nav-link-icon"><i class="fas fa-book-open"></i></div>
+                    Pages
+                    <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                </a>
+                <div class="collapse" id="collapsePages" aria-labelledby="headingTwo"
+                    data-bs-parent="#sidenavAccordion">
+                    <nav class="sb-sidenav-menu-nested nav accordion" id="sidenavAccordionPages">
+                        <a class="nav-link collapsed" href="#" data-bs-toggle="collapse"
+                            data-bs-target="#pagesCollapseAuth" aria-expanded="false"
+                            aria-controls="pagesCollapseAuth">
+                            Authentication
+                            <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                        </a>
+                        <div class="collapse" id="pagesCollapseAuth" aria-labelledby="headingOne"
+                            data-bs-parent="#sidenavAccordionPages">
+                            <nav class="sb-sidenav-menu-nested nav">
+                                <a class="nav-link" href="login.html">Login</a>
+                                <a class="nav-link" href="register.html">Register</a>
+                                <a class="nav-link" href="password.html">Forgot Password</a>
+                            </nav>
                         </div>
-                    </div>
-                    <div class="sb-sidenav-footer">
-                        <div class="small">Logged in as:</div>
-                        {{ Auth::user()->name }}
-                    </div>
-                </nav>
+                        <a class="nav-link collapsed" href="#" data-bs-toggle="collapse"
+                            data-bs-target="#pagesCollapseError" aria-expanded="false"
+                            aria-controls="pagesCollapseError">
+                            Error
+                            <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                        </a>
+                        <div class="collapse" id="pagesCollapseError" aria-labelledby="headingOne"
+                            data-bs-parent="#sidenavAccordionPages">
+                            <nav class="sb-sidenav-menu-nested nav">
+                                <a class="nav-link" href="401.html">401 Page</a>
+                                <a class="nav-link" href="404.html">404 Page</a>
+                                <a class="nav-link" href="500.html">500 Page</a>
+                            </nav>
+                        </div>
+                    </nav>
+                </div>
+                <div class="sb-sidenav-menu-heading">Addons</div>
+                <a class="nav-link" href="charts.html">
+                    <div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
+                    Charts
+                </a>
+                <a class="nav-link" href="tables.html">
+                    <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
+                    Tables
+                </a>
             </div>
+        </div>
+        <div class="sb-sidenav-footer">
+            <div class="small">Logged in as:</div>
+            {{-- {{ Auth::user()->name }} --}}
+        </div>
+    </nav>
+</div>
