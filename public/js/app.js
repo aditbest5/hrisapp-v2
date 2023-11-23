be8733be7f0d60781f6199ce783fe669853d7943
Muhammad Rifqi <muhammad45rifki@gmail.com>
2023-11-23 06:38:10 +0000

    update

--- public/js/app.js
+++ public/js/app.js
@@ -19437,8 +19437,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\xampp\htdocs\hrisapp-v2\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\xampp\htdocs\hrisapp-v2\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /var/www/html/hrisapp-v2/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /var/www/html/hrisapp-v2/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
